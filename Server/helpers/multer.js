5416bbe3b75c491df8439a00e812258e3be975de
BODHEESH <105903006+BODHEESH@users.noreply.github.com>
2022-12-26 15:14:02 +0530

    google key removed

--- Server/helpers/multer.js
+++ Server/helpers/multer.js
@@ -4,13 +4,39 @@ var {google} = require('googleapis')
 var GoogleDriveStorage = require("multer-google-drive")
 
 
+const storage = multer.diskStorage({
+    destination(req,file,callback){
+        callback(null,'./public/pdfFiles');
+    },
+    filename(req,file,callback){
+
+        if(!file.originalname.match(/\.(pdf)$/)) {
+            return callback( new Error('Please upload a valid pdf file'))
+        }else{
+            return callback(null,`${file.originalname}${Date.now()}${file.fieldname}${".pdf"}`);
+        }
+    },
+});
+
+
+const upload = multer({ storage:storage});
+
+
+
+
+
+
+module.exports = upload
+
+
+
 /* -------------------------------------------------------------------------- */
 /*              storage change to google drive under development              */
 /* -------------------------------------------------------------------------- */
 
 
-//   "google_api_folder_id": "1q3x5zt9yE_LLG_pQXpJ7Mr_NSX_hrA-X"
-// const GOOGLE_API_FOLDER_ID='1q3x5zt9yE_LLG_pQXpJ7Mr_NSX_hrA-X'
+//   "google_api_folder_id": ""
+// const GOOGLE_API_FOLDER_ID=''
 
 
 // const auth = new google.auth.GoogleAuth({
@@ -41,34 +67,3 @@ var GoogleDriveStorage = require("multer-google-drive")
       
     // }) 
 
-
-
-
-
-
-
-
-
-const storage = multer.diskStorage({
-    destination(req,file,callback){
-        callback(null,'./public/pdfFiles');
-    },
-    filename(req,file,callback){
-
-        if(!file.originalname.match(/\.(pdf)$/)) {
-            return callback( new Error('Please upload a valid pdf file'))
-        }else{
-            return callback(null,`${file.originalname}${Date.now()}${file.fieldname}${".pdf"}`);
-        }
-    },
-});
-
-
-const upload = multer({ storage:storage});
-
-
-
-
-
-
-module.exports = upload
